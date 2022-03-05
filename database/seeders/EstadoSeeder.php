1131af182b5c0bd9ebf4dfa15d3d42bd4a2a84f2
david1066 <davinchi1066@hotmail.com>
2022-03-04 20:01:56 -0500

    creando el seeder de estados

---
+++ database/seeders/EstadoSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Estado;
+
+class EstadoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Estado::create(["nombre"=>"Sin publicar"]);
+        Estado::create(["nombre"=>"Publicado"]);
+    }
+}
