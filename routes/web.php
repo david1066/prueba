8ba384758bfd21f09c5fa88c484a76ff84a3dfff
david1066 <davinchi1066@hotmail.com>
2022-03-04 20:02:36 -0500

    cambiando la ruta principal

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-/* Auth::routes(); */
+/*  */
 
 Route::middleware(['peticion'])->group(function () { 
 
-    
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Auth::routes();
+    Route::get('/admin/productos', [App\Http\Controllers\HomeController::class, 'index'])->name('admin.productos');
  });
